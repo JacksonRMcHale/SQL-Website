4d1cff36fe691ed205d73aa5c1d7da03525891d3
JacksonRMcHale <jmchale@students.kennesaw.edu>
2021-10-14 18:21:28 -0400

    add php

--- index.php
+++ index.php
@@ -6,6 +6,7 @@
             I am Jackson McHale. 
             I am a senior in software engineering and this website was 
             <b>Created for my SWE 4633 class.</b> 
+            <?php echo "Hello World!"; />
         </p>
         <a href="page2.html">Page 2</a>
     </body>
