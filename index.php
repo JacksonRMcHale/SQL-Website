ecf623105161835d07a4469ffbe6f54db79d480f
JacksonRMcHale <jmchale@students.kennesaw.edu>
2021-10-14 18:13:47 -0400

    only html

--- index.php
+++ index.php
@@ -1,8 +1,3 @@
-<?php
-    echo "Hello World!";
-/*
-
-<!--
 <!DOCTYPE html>
 <html>
     <body>
@@ -11,11 +6,7 @@
             I am Jackson McHale. 
             I am a senior in software engineering and this website was 
             <b>Created for my SWE 4633 class.</b> 
-            < ?php
-                echo "Hello World!";
-            />
         </p>
         <a href="page2.html">Page 2</a>
     </body>
 </html>
--->*/
