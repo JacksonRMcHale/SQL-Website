911168fc6df3f2c3493f17a95a1dee33763f33f5
JacksonRMcHale <jmchale@students.kennesaw.edu>
2021-10-28 12:19:47 -0400

    New php

--- index.php
+++ index.php
@@ -12,10 +12,22 @@
             Here's some data from the sql database:
             <?php
                 // SQL Server Extension Sample Code:
-                $connectionInfo = array("UID" => "jmchale", "pwd" => "3Ju9w43jxmPxjaV", "Database" => "grades", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
-                $serverName = "tcp:jmchale-swe4633-sql.database.windows.net,1433";
-                echo "\n1\n";
-                echo sqlsrv_connect($serverName, $connectionInfo);
+                # $connectionInfo = array("UID" => "jmchale", "pwd" => "3Ju9w43jxmPxjaV", "Database" => "grades", "LoginTimeout" => 30, "Encrypt" => 1, "TrustServerCertificate" => 0);
+                # $serverName = "tcp:jmchale-swe4633-sql.database.windows.net,1433";
+                # echo "\n1\n";
+                # echo sqlsrv_connect($serverName, $connectionInfo);
+                function OpenConnection()
+                {
+                    $serverName = "tcp:jmchale-swe4633-sql.database.windows.net,1433";
+                    $connectionOptions = array("Database"=>"grades",
+                        "Uid"=>"jmchale", "PWD"=>"3Ju9w43jxmPxjaV");
+                    $conn = sqlsrv_connect($serverName, $connectionOptions);
+                    if($conn == false)
+                        die(FormatErrors(sqlsrv_errors()));
+            
+                    return $conn;
+                }
+                OpenConnection();
                 echo "2\n";
                 sqlsrv_query($conn, "SELECT * FROM grades;");
                 echo "3\n";
